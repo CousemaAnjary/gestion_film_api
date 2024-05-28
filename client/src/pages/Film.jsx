796e76d9836e167a9b18ffe3f605da4238251147
CousemaAnjary <anjaryabdillah@gmail.com>
2024-05-28 21:58:43 +0300

    chore: Add axios dependency and update React version

---
+++ client/src/pages/Film.jsx
@@ -0,0 +1,13 @@
+function Film() {
+  // state (état, données) de l'application
+
+  // comportement
+
+  // affichage (render
+  return (
+    <>
+      <h1>La page film</h1>
+    </>
+  );
+}
+export default Film;
---
+++ client/src/services/filmService.js
@@ -0,0 +1,13 @@
+import api from "./apiConfig";
+
+// Recuperér la liste des films
+export const getFilms = async () => {
+  try {
+    const response = await api.get("/films");
+    return response.data;
+
+  } catch (error) {
+    console.error("Error fetching films:", error);
+    throw error;
+  }
+};
