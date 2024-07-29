c15bd62e1f4e77a69bcb45207a3b59aa6c241449
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-29 21:08:34 +0300

    chore: Update Home page component export statement

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 function Home() {
   // state (état, données) de l'application
 
+
   // comportement
+
 
   // affichage (render) 
   return (
     <>
       <h1>Bienvenue sur la page Home</h1>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
